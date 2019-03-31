94d07c1767448b3609c725ddc31f865e0db30742
ksakash <akashsingh049811@gmail.com>
2019-03-31 20:34:57 +0530

    added cascaded dp pid controller

--- uuv_control/uuv_trajectory_control/src/uuv_control_interfaces/dp_pid_controller_base.py
+++ uuv_control/uuv_trajectory_control/src/uuv_control_interfaces/dp_pid_controller_base.py
@@ -83,32 +83,6 @@ class DPPIDControllerBase(DPControllerBase):
 
         self._logger.info('PID controller ready!')
 
-        self._mass = rospy.get_param("pid/mass")
-        self._inertial = rospy.get_param("pid/inertial")
-
-        # update mass, moments of inertia
-        self._inertial_tensor = np.array(
-          [[self._inertial['ixx'], self._inertial['ixy'], self._inertial['ixz']],
-           [self._inertial['ixy'], self._inertial['iyy'], self._inertial['iyz']],
-           [self._inertial['ixz'], self._inertial['iyz'], self._inertial['izz']]])
-        self._mass_inertial_matrix = np.vstack((
-          np.hstack((self._mass*np.identity(3), np.zeros((3, 3)))),
-          np.hstack((np.zeros((3, 3)), self._inertial_tensor))))
-        
-        print self._mass_inertial_matrix
-
-        self._velocity_pid = rospy.get_param('velocity_control')
-        print self._velocity_pid
-
-        self._position_pid = rospy.get_param('position_control')
-        print self._position_pid
-
-        self._lin_vel_pid_reg = PIDRegulator(self._velocity_pid['linear_p'], self._velocity_pid['linear_i'], self._velocity_pid['linear_d'], 2)
-        self._ang_vel_pid_reg = PIDRegulator(0, 0, 0, 1)
-        self._lin_pos_pid_reg = PIDRegulator(self._position_pid['pos_p'], self._position_pid['pos_i'], self._position_pid['pos_d'], self._position_pid['pos_sat'])
-        self._ang_pos_pid_reg = PIDRegulator(self._position_pid['rot_p'], self._position_pid['rot_i'], self._position_pid['rot_d'], self._position_pid['rot_sat'])
-
-
     def _reset_controller(self):
         super(DPPIDControllerBase, self)._reset_controller()
         self._error_pose = np.zeros(6)
@@ -135,53 +109,11 @@ class DPPIDControllerBase(DPControllerBase):
         if not self.odom_is_init:
             return
 
-        return self.position_control()
-
         # Update integrator
-        # self._int += 0.5 * (self.error_pose_euler + self._error_pose) * self._dt
+        self._int += 0.5 * (self.error_pose_euler + self._error_pose) * self._dt
         # Store current pose error
-        # self._error_pose = self.error_pose_euler
-        # return np.dot(self._Kp, self.error_pose_euler) \
-        #     + np.dot(self._Kd, self._errors['vel']) \
-        #     + np.dot(self._Ki, self._int)
-
-    def position_control(self):
-
-        p = self._vehicle_model._pose['pos']
-        q = self._vehicle_model._pose['rot']
-
-        # Compute control output:
-        t = rospy.get_rostime().to_sec()
-
-        # Position error
-        e_pos_world = self._reference['pos'] - p
-        e_pos_body = trans.quaternion_matrix(q).transpose()[0:3,0:3].dot(e_pos_world)
-
-        # Error quaternion wrt body frame
-        e_rot_quat = trans.quaternion_multiply(trans.quaternion_conjugate(q), self._reference['rot'])
-
-        if np.linalg.norm(e_pos_world[0:2]) > 5.0:
-            # special case if we are far away from goal:
-            # ignore desired heading, look towards goal position
-            heading = math.atan2(e_pos_world[1],e_pos_world[0])
-            quat_des = np.array([0, 0, math.sin(0.5*heading), math.cos(0.5*heading)])
-            e_rot_quat = trans.quaternion_multiply(trans.quaternion_conjugate(q), quat_des)
-
-        # Error angles
-        e_rot = np.array(trans.euler_from_quaternion(e_rot_quat))
-
-        v_linear = self._lin_pos_pid_reg.regulate(e_pos_body, t)
-        v_angular = self._ang_pos_pid_reg.regulate(e_rot, t)
-
-        e_linear_vel = v_linear - np.array([self._vehicle_model._vel[0], self._vehicle_model._vel[1], self._vehicle_model._vel[2]])
-        e_angular_vel = v_angular - np.array([self._vehicle_model._vel[3], self._vehicle_model._vel[4], self._vehicle_model._vel[5]])
-
-        a_linear = self._lin_vel_pid_reg.regulate(e_linear_vel, t)
-        
-        a_angular = self._ang_vel_pid_reg.regulate(e_angular_vel, t)
-
-        accel = np.hstack((a_linear, a_angular)).transpose()
-        force_torque = self._mass_inertial_matrix.dot(accel)
-
-        return force_torque
+        self._error_pose = self.error_pose_euler
+        return np.dot(self._Kp, self.error_pose_euler) \
+            + np.dot(self._Kd, self._errors['vel']) \
+            + np.dot(self._Ki, self._int)
         
